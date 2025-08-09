5129dcb16e0edf5261fe01c57890e7bf84eb8a51
jerryfrancis-97 <francis.jerome999@gmail.com>
2025-08-09 18:59:35 -0400

    restart code process

--- design-to-code-ui/src/app/page.tsx
+++
@@ -1,5 +0,0 @@
-import DesignWorkspace from '../components/DesignWorkspace';
-
-export default function Home() {
-  return <DesignWorkspace />;
-}
--- design-to-code-ui/src/components/index.ts
+++
@@ -1,6 +0,0 @@
-export { default as DesignWorkspace } from './DesignWorkspace';
-export { default as FlowCanvas } from './FlowCanvas';
-export { default as AISidebar } from './AISidebar';
-export { default as Toolbar } from './Toolbar';
-export { default as DemoMode } from './DemoMode';
-export * from './ui-components';
